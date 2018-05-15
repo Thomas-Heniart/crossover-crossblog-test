523d0b407fb677e787ab3e02ba19d641b84df234
Thomas Heniart <heniart.thomas@gmail.com>
2018-05-15 16:28:09 +0200

    Check old method time

---
+++ src/main/java/com/crossover/techtrial/bootstrap/ArticleBootstrap.java
@@ -0,0 +1,40 @@
+package com.crossover.techtrial.bootstrap;
+
+import com.crossover.techtrial.model.Article;
+import com.crossover.techtrial.repository.ArticleRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class ArticleBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ArticleRepository articleRepository;
+
+    private static final String[] titles = {
+            "Title",
+            "Title1",
+            "Title2"
+    };
+
+    public ArticleBootstrap(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        this.loadArticles();
+    }
+
+    private void loadArticles() {
+        this.articleRepository.deleteAll();
+        for (int i = 0; i < 10000; i++) {
+            Article article = new Article();
+            article.setEmail("heniart.thomas@gmail.com");
+            article.setTitle(titles[ThreadLocalRandom.current().nextInt(titles.length)]);
+            this.articleRepository.save(article);
+        }
+    }
+}
--- src/main/java/com/crossover/techtrial/controller/ArticleController.java
+++ src/main/java/com/crossover/techtrial/controller/ArticleController.java
@@ -1,59 +1,54 @@
 package com.crossover.techtrial.controller;
 
-import java.util.List;
-
+import com.crossover.techtrial.model.Article;
+import com.crossover.techtrial.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.crossover.techtrial.model.Article;
-import com.crossover.techtrial.service.ArticleService;
+import java.util.List;
 
 @RestController
 public class ArticleController {
 
-	@Autowired
-	ArticleService articleService;
+    @Autowired
+    ArticleService articleService;
 
-	@PostMapping(path = "articles")
-	public ResponseEntity<Article> createArticle(@RequestBody Article article) {
-		return new ResponseEntity<>(articleService.save(article), HttpStatus.CREATED);
-	}
+    @PostMapping(path = "articles")
+    public ResponseEntity<Article> createArticle(@RequestBody Article article) {
+        return new ResponseEntity<>(articleService.save(article), HttpStatus.CREATED);
+    }
 
-	@GetMapping(path = "articles/{article-id}")
-	public ResponseEntity<Article> getArticleById(@PathVariable("article-id") Long id) {
-		Article article = articleService.findById(id);
-		if (article != null)
-			return new ResponseEntity<>(article, HttpStatus.OK);
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
+    @GetMapping(path = "articles/{article-id}")
+    public ResponseEntity<Article> getArticleById(@PathVariable("article-id") Long id) {
+        Article article = articleService.findById(id);
+        if (article != null)
+            return new ResponseEntity<>(article, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
-	@PutMapping(path = "articles/{article-id}")
-	public ResponseEntity<Article> updateArticle(@PathVariable("article-id") Long id, @RequestBody Article article) {
-		return new ResponseEntity<>(articleService.save(article), HttpStatus.OK);
-	}
+    @PutMapping(path = "articles/{article-id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable("article-id") Long id, @RequestBody Article article) {
+        return new ResponseEntity<>(articleService.save(article), HttpStatus.OK);
+    }
 
-	@DeleteMapping(path = "articles/{article-id}")
-	public ResponseEntity<Article> deleteArticleById(@PathVariable("article-id") Long id) {
-		articleService.delete(id);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+    @DeleteMapping(path = "articles/{article-id}")
+    public ResponseEntity<Article> deleteArticleById(@PathVariable("article-id") Long id) {
+        articleService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
-	/*
-	 * Search endpoint is used in autocomplete and return only top 10 results.
-	 * Therefore no need for pagination.
-	 */
-	@GetMapping(path = "articles/search")
-	public ResponseEntity<List<Article>> searchArticles(@RequestParam(value = "text") String text) {
-		return new ResponseEntity<>(articleService.search(text), HttpStatus.OK);
-	}
+    /*
+     * Search endpoint is used in autocomplete and return only top 10 results.
+     * Therefore no need for pagination.
+     */
+    @GetMapping(path = "articles/search")
+    public ResponseEntity<List<Article>> searchArticles(@RequestParam(value = "text") String text) {
+        long startTime = System.currentTimeMillis();
+        ResponseEntity<List<Article>> responseEntity = new ResponseEntity<>(articleService.search(text), HttpStatus.OK);
+        System.out.println("Method time: " + (System.currentTimeMillis() - startTime));
+        return responseEntity;
+    }
 
 }
